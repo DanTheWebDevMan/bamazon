f8623492c8f897beee182711f632acdb1dee8ef8
Danniel Zelaya <dannielzelaya@gmail.com>
2019-08-16 20:03:57 -0500

    created node application

--- bamazon.sql
+++ bamazon.sql
@@ -12,4 +12,7 @@ CREATE TABLE products (
 	stock_qty INTEGER ,
 	PRIMARY KEY (id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_qty)
+VALUES ("watch", "jewelery", 125.00, 10) , ("mouse", "electronics", 10.00, 50), ("shirt", "clothes", 15.99, 25), ("iphone", "electronics", 999.99, 50), ("macbook", "electronics", 1500.00, 5), ("stuffed_monkey", "toys", 7.99, 30), ("cofee", "food", 7.99, 48), ("table", "furniture", 249.00, 3), ("chair", "furniture", 69.99, 12);
 	
