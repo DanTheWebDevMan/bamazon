4d27882aff706b6883c1d5a4cf09bf6da5c58db6
Danniel Zelaya <dannielzelaya@gmail.com>
2019-08-16 20:28:28 -0500

    table displays correctly in workbench

--- bamazon.sql
+++ bamazon.sql
@@ -14,5 +14,5 @@ CREATE TABLE products (
 );
 
 INSERT INTO products (product_name, department_name, price, stock_qty)
-VALUES ("watch", "jewelery", 125.00, 10) , ("mouse", "electronics", 10.00, 50), ("shirt", "clothes", 15.99, 25), ("iphone", "electronics", 999.99, 50), ("macbook", "electronics", 1500.00, 5), ("stuffed_monkey", "toys", 7.99, 30), ("cofee", "food", 7.99, 48), ("table", "furniture", 249.00, 3), ("chair", "furniture", 69.99, 12);
+VALUES ("watch", "jewelery", 125.00, 10) , ("mouse", "electronics", 10.00, 50), ("shirt", "clothes", 15.99, 25), ("iphone", "electronics", 999.99, 50), ("macbook", "electronics", 999.00, 5), ("stuffed_monkey", "toys", 7.99, 30), ("cofee", "food", 7.99, 48), ("table", "furniture", 249.00, 3), ("chair", "furniture", 69.99, 12);
 	
---
+++ bamazonCustomer.js
@@ -0,0 +1,43 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "root",
+
+  // Your port; if not 3306
+  port: 3307,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "bamazon"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+  });
+
+  // function which prompts the user for what action they should take
+  var displayProducts = function(){
+	var query = "Select * FROM products";
+	connection.query(query, function(err, res){
+		if(err) throw err;
+		var displayTable = new Table ({
+			head: ["Item ID", "Product Name", "Category", "Price", "Quantity"],
+			colWidths: [10,25,25,10,14]
+		});
+		for(var i = 0; i < res.length; i++){
+			displayTable.push(
+				[res[i].item_id,res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+				);
+		}
+		console.log(displayTable.toString());
+		purchasePrompt();
+	});
+}
